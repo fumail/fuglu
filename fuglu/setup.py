8b7695d438217279563c0dfd24f9f757636951e5
gryphius <oli@fuglu.org>
2009-06-26 08:11:17 +0000

    initial import

---
+++ fuglu/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+import glob
+
+
+setup(name = "fuglu",
+    version = "0.4.1svn",
+    description = "Fuglu Mail Content Scanner",
+    author = "O. Schacher",
+    url='http://www.fuglu.org',
+    author_email = "oli@fuglu.org",
+    package_dir={'':'src'},
+    packages = ['fuglu','fuglu.plugins','fuglu.extensions'],
+    scripts = ["src/startscript/fuglu","src/tools/fuglu_debug"],
+    long_description = """Fuglu Mail Content Scanner""" ,
+    data_files=[
+                ('/etc/fuglu',glob.glob('conf/*.dist')),
+                ('/etc/fuglu/templates',glob.glob('conf/templates/*.dist')),
+                ('/etc/fuglu/rules',glob.glob('conf/rules/*.dist')),
+                ('/etc/init.d',['scripts/init.d-centos/fuglu']),
+                ]
+) 
