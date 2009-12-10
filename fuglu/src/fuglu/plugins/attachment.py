4d29772df90bb45487afb97d9c84fabc464e2256
gryphius <oli@fuglu.org>
2009-12-10 08:57:48 +0000

    fixed indentation

--- fuglu/src/fuglu/plugins/attachment.py
+++ fuglu/src/fuglu/plugins/attachment.py
@@ -3,7 +3,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
@@ -33,124 +33,124 @@ try:
     MAGIC_AVAILABLE=True
 except ImportError:
     pass
-    
+
 
 FUATT_NAMESCONFENDING="-filenames.conf"
 FUATT_CTYPESCONFENDING="-filetypes.conf"
 
 
 class RulesCache( object ):
-   """caches rule files and compiled regex patterns"""
-    
-   __shared_state = {}
-   
-   def __init__(self,rulesdir):
-       self.__dict__ = self.__shared_state
-       if not hasattr(self, 'rules'):
-           self.rules={}
-       if not hasattr(self,'regexcache'):
-           self.regexcache={}
-           
-       if not hasattr(self, 'lock'):
-           self.lock=Lock()
-       if not hasattr(self,'logger'):
-           self.logger=logging.getLogger('fuglu.plugin.FiletypePlugin.RulesCache')
-       if not hasattr(self,'lastreload'):
-           self.lastreload=0
-       self.rulesdir=rulesdir
-       self.reloadifnecessary()
-       
-       
-   
-   
-   def getRegex(self,regex):
-       """compile regex and return cached object"""
-       if self.regexcache.has_key(regex):
-           return self.regexcache[regex]
-       try:
-           prog=re.compile(regex, re.IGNORECASE)
-       except Exception,e:
-           self.logger.error('Regex compilation error for %s : %s'%(regex,e))
-       self.regexcache[regex]=prog
-       return prog
-   
-   def getRules(self,type,key):
-       self.logger.debug('Rule cache request: [%s] [%s]'%(type,key))
-       if not self.rules.has_key(type):
-           self.logger.error('Invalid rule type requested: %s'%type)
-       if not self.rules[type].has_key(key):
-           self.logger.debug('Ruleset not found : [%s] [%s]'%(type,key))
-           return None
-       self.logger.debug('Ruleset found : [%s] [%s] '%(type,key))
-       
-       ret=self.rules[type][key]
-       return ret
-   
-   def getCTYPERules(self,key):
-       return self.getRules('ctype', key)
-      
-   def getNAMERules(self,key):
-       return self.getRules('name', key)
-       
-   def reloadifnecessary(self):
-       """reload rules if file changed"""
-       if not self.rulesdirchanged():
-           return
-       if not self.lock.acquire():
-               return
-       try:
-           self._loadrules()
-       finally:
-           self.lock.release()
-   
-   def rulesdirchanged(self):
-       statinfo=os.stat(self.rulesdir)
-       ctime=statinfo.st_ctime
-       if ctime>self.lastreload:
-           return True
-       return False
+    """caches rule files and compiled regex patterns"""
 
-   def _loadrules(self):
-       """effectively loads the rules, do not call directly, only through reloadifnecessary"""
-       self.logger.debug('Re-Loading attachment rules...')
-       
-       #set last timestamp
-       statinfo=os.stat(self.rulesdir)
-       ctime=statinfo.st_ctime
-       self.lastreload=ctime
-       
-       
-       filelist=os.listdir(self.rulesdir)
-       
-       newruleset={'name':{},'ctype':{}}
-       
-       rulecounter=0
-       for filename in filelist:
-           if  not (filename.endswith(FUATT_NAMESCONFENDING) or filename.endswith(FUATT_CTYPESCONFENDING)):
-               self.logger.debug('Ignoring file %s'%filename)
-               continue
-           
-           
-           ruleset=self._loadonefile("%s/%s"%(self.rulesdir,filename))
-           if ruleset==None:
-               continue
-           rulesloaded=len(ruleset)
-           self.logger.debug('%s rules loaded from file %s'%(rulesloaded,filename))
-           type='name'
-           key=filename[0:-len(FUATT_NAMESCONFENDING)]
-           if(filename.endswith(FUATT_CTYPESCONFENDING)):
-              type='ctype'
-              key=filename[0:-len(FUATT_CTYPESCONFENDING)]
-           newruleset[type][key]=ruleset
-           self.logger.debug('Updating cache: [%s][%s]'%(type,key))
-           rulecounter+=rulesloaded
-       
-       totalfiles=len(filelist)
-       self.rules=newruleset
-       self.logger.info('Loaded %s rules from %s files'%(rulecounter,totalfiles))
+    __shared_state = {}
 
-   
-   def _loadonefile(self,filename):
+    def __init__(self,rulesdir):
+        self.__dict__ = self.__shared_state
+        if not hasattr(self, 'rules'):
+            self.rules={}
+        if not hasattr(self,'regexcache'):
+            self.regexcache={}
+
+        if not hasattr(self, 'lock'):
+            self.lock=Lock()
+        if not hasattr(self,'logger'):
+            self.logger=logging.getLogger('fuglu.plugin.FiletypePlugin.RulesCache')
+        if not hasattr(self,'lastreload'):
+            self.lastreload=0
+        self.rulesdir=rulesdir
+        self.reloadifnecessary()
+
+
+
+
+    def getRegex(self,regex):
+        """compile regex and return cached object"""
+        if self.regexcache.has_key(regex):
+            return self.regexcache[regex]
+        try:
+            prog=re.compile(regex, re.IGNORECASE)
+        except Exception,e:
+            self.logger.error('Regex compilation error for %s : %s'%(regex,e))
+        self.regexcache[regex]=prog
+        return prog
+
+    def getRules(self,type,key):
+        self.logger.debug('Rule cache request: [%s] [%s]'%(type,key))
+        if not self.rules.has_key(type):
+            self.logger.error('Invalid rule type requested: %s'%type)
+        if not self.rules[type].has_key(key):
+            self.logger.debug('Ruleset not found : [%s] [%s]'%(type,key))
+            return None
+        self.logger.debug('Ruleset found : [%s] [%s] '%(type,key))
+
+        ret=self.rules[type][key]
+        return ret
+
+    def getCTYPERules(self,key):
+        return self.getRules('ctype', key)
+
+    def getNAMERules(self,key):
+        return self.getRules('name', key)
+
+    def reloadifnecessary(self):
+        """reload rules if file changed"""
+        if not self.rulesdirchanged():
+            return
+        if not self.lock.acquire():
+            return
+        try:
+            self._loadrules()
+        finally:
+            self.lock.release()
+
+    def rulesdirchanged(self):
+        statinfo=os.stat(self.rulesdir)
+        ctime=statinfo.st_ctime
+        if ctime>self.lastreload:
+            return True
+        return False
+
+    def _loadrules(self):
+        """effectively loads the rules, do not call directly, only through reloadifnecessary"""
+        self.logger.debug('Re-Loading attachment rules...')
+
+        #set last timestamp
+        statinfo=os.stat(self.rulesdir)
+        ctime=statinfo.st_ctime
+        self.lastreload=ctime
+
+
+        filelist=os.listdir(self.rulesdir)
+
+        newruleset={'name':{},'ctype':{}}
+
+        rulecounter=0
+        for filename in filelist:
+            if  not (filename.endswith(FUATT_NAMESCONFENDING) or filename.endswith(FUATT_CTYPESCONFENDING)):
+                self.logger.debug('Ignoring file %s'%filename)
+                continue
+
+
+            ruleset=self._loadonefile("%s/%s"%(self.rulesdir,filename))
+            if ruleset==None:
+                continue
+            rulesloaded=len(ruleset)
+            self.logger.debug('%s rules loaded from file %s'%(rulesloaded,filename))
+            type='name'
+            key=filename[0:-len(FUATT_NAMESCONFENDING)]
+            if(filename.endswith(FUATT_CTYPESCONFENDING)):
+                type='ctype'
+                key=filename[0:-len(FUATT_CTYPESCONFENDING)]
+            newruleset[type][key]=ruleset
+            self.logger.debug('Updating cache: [%s][%s]'%(type,key))
+            rulecounter+=rulesloaded
+
+        totalfiles=len(filelist)
+        self.rules=newruleset
+        self.logger.info('Loaded %s rules from %s files'%(rulecounter,totalfiles))
+
+
+    def _loadonefile(self,filename):
         """returns all rules in a file"""
         if not os.path.exists(filename):
             self.logger.error('Rules File %s does not exist'%filename)
@@ -172,7 +172,7 @@ class RulesCache( object ):
             if action!="allow" and action!="deny" and action!="delete":
                 self.logger.error('Invalid rule action: %s'%action)
                 continue
-            
+
             tp=(action,regex,description)
             ret[regex]=tp
         return ret
@@ -190,59 +190,59 @@ class FiletypePlugin(ScannerPlugin):
             self.logger.warning('python-magic not available')
         self.rulescache=RulesCache(self.config.get('FiletypePlugin','rulesdir'))
         self.extremeverbosity=False
-    
- 
-    def examine(self,suspect):  
+
+
+    def examine(self,suspect):
         starttime=time.time()
         self.blockedfiletemplate=self.config.get('FiletypePlugin','template_blockedfile')
-        
+
         returnaction=self.walk(suspect)
-        
+
 
         endtime=time.time()
         difftime=endtime-starttime
         suspect.tags['FiletypePlugin.time']="%.4f"%difftime
         return returnaction
-    
+
     def getFiletype(self,path):
         type =  self.ms.file(path)
         return type
-    
+
     def getBuffertype(self,buffer):
         type=self.ms.buffer(buffer)
         return type
-    
+
 
     def matchRules(self,ruleset,object,suspect):
         if ruleset==None:
             return DUNNO
-        
+
         for regex in ruleset.keys():
-                prog=self.rulescache.getRegex(regex)
-                if self.extremeverbosity:
-                    self._logger().debug('Attachment %s Rule %s'%(object,regex))
-                if prog.search( object):
-                    info=ruleset[regex]
-                    action=info[0]
-                    description=info[2]
-                    self._logger().debug('Rulematch: Attachment=%s Rule=%s Description=%s Action=%s'%(object,regex,description,action))
-                    suspect.debug('Rulematch: Attachment=%s Rule=%s Description=%s Action=%s'%(object,regex,description,action))
-                    if action=='deny':
-                        # remove non ascii chars
-                        asciirep="".join([x for x in object if ord(x) < 128])
-                        self._logger().info('Mail contains blocked attachment name/type %s (delete, send bounce) '%object)
-                        blockinfo="%s: %s"%(asciirep,description)
-                        suspect.tags['FiletypePlugin.errormessage']=blockinfo
-                        bounce=Bounce(self.config)
-                        bounce.send_template_file(suspect.from_address, self.blockedfiletemplate, suspect,dict(blockinfo=blockinfo))
-                        return DELETE
-                    
-                    if action=='delete':
-                        self._logger().info('Mail contains blocked attachment name/type %s (delete, no bounce)'%object)
-                        return DELETE
-                    
-                    if action=='accept':
-                        return 'accept'
+            prog=self.rulescache.getRegex(regex)
+            if self.extremeverbosity:
+                self._logger().debug('Attachment %s Rule %s'%(object,regex))
+            if prog.search( object):
+                info=ruleset[regex]
+                action=info[0]
+                description=info[2]
+                self._logger().debug('Rulematch: Attachment=%s Rule=%s Description=%s Action=%s'%(object,regex,description,action))
+                suspect.debug('Rulematch: Attachment=%s Rule=%s Description=%s Action=%s'%(object,regex,description,action))
+                if action=='deny':
+                    # remove non ascii chars
+                    asciirep="".join([x for x in object if ord(x) < 128])
+                    self._logger().info('Mail contains blocked attachment name/type %s (delete, send bounce) '%object)
+                    blockinfo="%s: %s"%(asciirep,description)
+                    suspect.tags['FiletypePlugin.errormessage']=blockinfo
+                    bounce=Bounce(self.config)
+                    bounce.send_template_file(suspect.from_address, self.blockedfiletemplate, suspect,dict(blockinfo=blockinfo))
+                    return DELETE
+
+                if action=='delete':
+                    self._logger().info('Mail contains blocked attachment name/type %s (delete, no bounce)'%object)
+                    return DELETE
+
+                if action=='accept':
+                    return 'accept'
         return DUNNO
 
 
@@ -254,84 +254,84 @@ class FiletypePlugin(ScannerPlugin):
             if res!=DUNNO:
                 return res
         return DUNNO
-    
+
     def walk(self,suspect):
         """walks through a message and checks each attachment according to the rulefile specified in the config"""
         self.rulescache.reloadifnecessary()
         user_names=self.rulescache.getNAMERules(suspect.to_address)
-        
-        
+
+
         user_ctypes=self.rulescache.getCTYPERules(suspect.to_address)
-        
+
         domain_names=self.rulescache.getNAMERules(suspect.to_domain)
         domain_ctypes=self.rulescache.getCTYPERules(suspect.to_domain)
-        
+
         default_names=self.rulescache.getNAMERules('default')
         default_ctypes=self.rulescache.getCTYPERules('default')
-        
+
         m=suspect.getMessageRep()
         for i in m.walk():
             if i.is_multipart():
                 continue
             contenttype_mime=i.get_content_type()
             att_name = i.get_filename(None)
-            
+
             if not att_name:
                 #workaround for mimetypes, it always takes .ksh for text/plain
                 if i.get_content_type()=='text/plain':
                     ext='.txt'
                 else:
                     ext = mimetypes.guess_extension(i.get_content_type())
-                
+
                 if ext==None:
                     ext=''
                 att_name = 'noname%s' % ext
-                
+
             #check attachment name
             #self._logger().debug('Attachment Name: %s'%att_name)
-            
-            
+
+
             res=self.matchMultipleSets([user_names,domain_names,default_names], att_name,suspect)
             if res==DELETE:
                 return DELETE
-                        
+
             #go through content type rules
-             
-            
+
+
             res=self.matchMultipleSets([user_ctypes,domain_ctypes,default_ctypes], contenttype_mime,suspect)
             if res==DELETE:
                 return DELETE
-            
+
             if MAGIC_AVAILABLE:
                 pl = i.get_payload(decode=True)
                 contenttype_magic=self.getBuffertype(pl)
                 res=self.matchMultipleSets([user_ctypes,domain_ctypes,default_ctypes], contenttype_magic,suspect)
                 if res==DELETE:
                     return DELETE
-            
+
         return DUNNO
-        
+
     def __str__(self):
         return "Attachment Blocker"
-    
+
     def lint(self):
         allok=(self.checkConfig() and self.lint_magic())
         return allok
-    
+
     def lint_magic(self):
         if not MAGIC_AVAILABLE:
             print "python-magic library not available. Will only do content-type checks, no real file analysis"
             return False
         return True
-    
-    
+
+
 class AttachmentPluginTestCase(unittest.TestCase):
     """Testcases for the Attachment Checker Plugin"""
     def setUp(self):
         from ConfigParser import RawConfigParser
         import tempfile
         import shutil
-        
+
         self.tempdir=tempfile.mkdtemp('attachtest', 'fuglu')
         self.template='%s/blockedfile.tmpl'%self.tempdir
         shutil.copy('../conf/templates/blockedfile.tmpl.dist',self.template)
@@ -344,24 +344,24 @@ class AttachmentPluginTestCase(unittest.TestCase):
         config.add_section('main')
         config.set('main','disablebounces','1')
         self.candidate=FiletypePlugin(config)
-        
-        
+
+
     def tearDown(self):
         os.remove('%s/default-filenames.conf'%self.tempdir)
         os.remove('%s/default-filetypes.conf'%self.tempdir)
         os.remove(self.template)
         os.rmdir(self.tempdir)
-        
+
     def test_hiddenbinary(self):
         """Test if hidden binaries get detected correctly"""
         import tempfile
         import shutil
-        
+
         #copy file rules
         tempfilename=tempfile.mktemp(suffix='virus', prefix='fuglu-unittest', dir='/tmp')
         shutil.copy('testdata/binaryattachment.eml',tempfilename)
-        suspect=Suspect('sender@unittests.fuglu.org','recipient@unittests.fuglu.org',tempfilename)   
-        
+        suspect=Suspect('sender@unittests.fuglu.org','recipient@unittests.fuglu.org',tempfilename)
+
         result=self.candidate.examine(suspect)
         os.remove(tempfilename)
         self.failIf(result!=DELETE)
@@ -371,11 +371,11 @@ class AttachmentPluginTestCase(unittest.TestCase):
         disabled - need new sample"""
         import tempfile
         import shutil
-        
+
         tempfilename=tempfile.mktemp(suffix='virus', prefix='fuglu-unittest', dir='/tmp')
         shutil.copy('testdata/utf8message.eml',tempfilename)
-        suspect=Suspect('sender@unittests.fuglu.org','recipient@unittests.fuglu.org',tempfilename)   
-        
+        suspect=Suspect('sender@unittests.fuglu.org','recipient@unittests.fuglu.org',tempfilename)
+
         result=self.candidate.examine(suspect)
         os.remove(tempfilename)
         self.failIf(result!=DUNNO)
