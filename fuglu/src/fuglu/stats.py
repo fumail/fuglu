a3f460ce5c8c86218290d987125b32cd28fcdebb
gryphius <oli@fuglu.org>
2010-02-20 23:08:04 +0000

    svn:id tag added

--- fuglu/src/fuglu/stats.py
+++ fuglu/src/fuglu/stats.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# $Id: core.py 81 2010-02-17 08:43:33Z gryphius $
+# $Id$
 #
 
 import time
