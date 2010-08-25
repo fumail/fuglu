50278795349bf7cd7e8bd9c1b278f201cd16f912
gryphius <oli@fuglu.org>
2010-08-25 08:51:21 +0000

    fix 'virusaction' setting

--- fuglu/src/fuglu/plugins/clamav.py
+++ fuglu/src/fuglu/plugins/clamav.py
@@ -14,10 +14,12 @@
 #
 # $Id$
 #
-from fuglu.shared import ScannerPlugin,string_to_actioncode,DEFER,DUNNO
+from fuglu.shared import ScannerPlugin,string_to_actioncode,DEFER,DUNNO,actioncode_to_string,\
+    DELETE, Suspect
 import socket
 import string
 import time
+import unittest
 
 
 
@@ -66,7 +68,9 @@ class ClamavPlugin(ScannerPlugin):
                 suspect.tags['ClamavPlugin.time']="%.4f"%difftime
                 
                 if viri!=None:
-                    return string_to_actioncode(self.config.get(self.section,'virusaction'))
+                    virusaction=self.config.get(self.section,'virusaction')
+                    actioncode=string_to_actioncode(virusaction,self.config)
+                    return actioncode
                 return DUNNO
             except Exception,e:
                 self._logger().warning("Error encountered while contacting clamd (try %s of %s): %s"%(i+1,self.retries,str(e)))
@@ -206,6 +210,8 @@ class ClamavPlugin(ScannerPlugin):
         return s
     
     def lint(self):
+        viract=self.config.get(self.section,'virusaction')
+        print "Virusaction: %s"%actioncode_to_string(string_to_actioncode(viract,self.config))
         allok=(self.checkConfig() and self.lint_ping() and self.lint_eicar())
         return allok
     
@@ -253,3 +259,57 @@ AAEAAQA3AAAAbQAAAAAA
             return False
         print "Clamav found virus",result
         return True
+    
+    
+class ClamavPluginTestCase(unittest.TestCase):
+    """Testcases for the Stub Plugin"""
+    def setUp(self):
+        from ConfigParser import RawConfigParser  
+        import os      
+        config=RawConfigParser()
+        config.add_section('main')
+        config.add_section('virus')
+        config.set('main','prependaddedheaders','X-Fuglu-')
+        config.set('virus','defaultvirusaction','DELETE')
+        config.add_section('ClamavPlugin')
+        config.set('ClamavPlugin', 'host','127.0.0.1')
+        config.set('ClamavPlugin', 'port','3310')
+        config.set('ClamavPlugin', 'timeout','5')
+        config.set('ClamavPlugin', 'retries','3')
+        config.set('ClamavPlugin', 'maxsize','22000000')
+        config.set('ClamavPlugin', 'virusaction','DEFAULTVIRUSACTION')
+        config.set('ClamavPlugin', 'problemaction','DEFER')
+
+        self.candidate=ClamavPlugin(config)
+
+    def test_score(self):
+        import email
+        suspect=Suspect('sender@unittests.fuglu.org','recipient@unittests.fuglu.org','/dev/null')
+        stream="""Date: Mon, 08 Sep 2008 17:33:54 +0200
+To: oli@unittests.fuglu.org
+From: oli@unittests.fuglu.org
+Subject: test eicar attachment
+X-Mailer: swaks v20061116.0 jetmore.org/john/code/#swaks
+MIME-Version: 1.0
+Content-Type: multipart/mixed; boundary="----=_MIME_BOUNDARY_000_12140"
+
+------=_MIME_BOUNDARY_000_12140
+Content-Type: text/plain
+
+Eicar test
+------=_MIME_BOUNDARY_000_12140
+Content-Type: application/octet-stream
+Content-Transfer-Encoding: BASE64
+Content-Disposition: attachment
+
+UEsDBAoAAAAAAGQ7WyUjS4psRgAAAEYAAAAJAAAAZWljYXIuY29tWDVPIVAlQEFQWzRcUFpYNTQo
+UF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCoNClBLAQIU
+AAoAAAAAAGQ7WyUjS4psRgAAAEYAAAAJAAAAAAAAAAEAIAD/gQAAAABlaWNhci5jb21QSwUGAAAA
+AAEAAQA3AAAAbQAAAAAA
+
+------=_MIME_BOUNDARY_000_12140--"""
+
+        suspect.setMessageRep(email.message_from_string(stream))
+        result=self.candidate.examine(suspect)
+        strresult=actioncode_to_string(result)
+        self.assertEqual(strresult,"DELETE")
