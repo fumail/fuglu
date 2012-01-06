1d49d6f159581ba94292ac1bb187194becffcf23
gryphius <oli@fuglu.org>
2012-01-06 10:22:43 +0000

    distro specific installation command (not yet working, just a placeholder)

--- fuglu/setup.py
+++ fuglu/setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from distutils.core import setup, Command
 import glob
 import sys
 sys.path.insert(0,'src')
@@ -20,3 +20,33 @@ setup(name = "fuglu",
                 ('/etc/fuglu/rules',glob.glob('conf/rules/*.dist')),
                 ]
 )
+
+
+class DistroDefault(Command):
+    description = "install distribuction specific init scripts and default config"
+    user_options = []
+    
+    def initialize_options(self):
+        pass
+        
+        
+    def finalize_options(self):
+        self.distroalias={
+            'arch':'arch',
+            'debian':'debian',
+            'ubuntu':'debian',
+            'mint':'debian',
+            'rhel':'rhel',
+            'centos':'rhel',
+        }
+        self.distro,self.version=self.detect_distro()
+        
+    def run(self):
+        assert self.distro!=None,"could not detect distribution"
+        if self.distro not in self.distroalias:
+            print "sorry, no defaults"
+    
+    def detect_distro(self):
+        """returns a distribution name and version number if possible"""
+        pass
+        
