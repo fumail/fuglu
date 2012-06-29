4cc1d12f38122536136d27dd66500d98616daccc
gryphius <oli@fuglu.org>
2012-06-29 07:23:51 +0000

    add f-prot to init

--- fuglu/src/fuglu/plugins/__init__.py
+++ fuglu/src/fuglu/plugins/__init__.py
@@ -14,4 +14,4 @@
 #
 # $Id$
 #
-__all__ = ['archive','attachment','clamav','sa','headers','p_debug','p_skipper','vacation','icap','sssp']
+__all__ = ['archive','attachment','clamav','sa','headers','p_debug','p_skipper','vacation','icap','sssp','fprot']
