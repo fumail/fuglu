8686fe27ffce7b1e45a62d0759425a1803597c8a
gryphius <oli@fuglu.org>
2012-09-28 11:56:07 +0200

    basic milter protocol should work now (ok, reject, defer, discard). no message modifications.

--- fuglu/src/fuglu/connectors/milterconnector.py
+++ fuglu/src/fuglu/connectors/milterconnector.py
@@ -21,12 +21,13 @@ import struct
 import binascii
 import traceback
 
-from fuglu.lib.ppymilterbase import PpyMilter,PpyMilterDispatcher,PpyMilterCloseConnection
+from fuglu.lib.ppymilterbase import PpyMilter,PpyMilterDispatcher,PpyMilterCloseConnection,SMFIC_BODYEOB,RESPONSE
 
 from fuglu.scansession import SessionHandler
 from fuglu.shared import Suspect
 from fuglu.protocolbase import ProtocolHandler,BasicTCPServer
-
+import tempfile
+import os
 
 MILTER_LEN_BYTES = 4  # from sendmail's include/libmilter/mfdef.h
 class MilterHandler(ProtocolHandler):
@@ -42,17 +43,46 @@ class MilterHandler(ProtocolHandler):
             self.logger.error('MILTER SESSION NOT COMPLETED')
             return None
         
-        #TODO: check success
-
         sess=self.sess
         fromaddr=sess.from_address
         toaddr=sess.to_address
         tempfilename=sess.tempfilename
-
+        body=open(tempfilename).read()
+        self.logger.info("BODY=%s"%body)
+        self.logger.info("MILTER: from=%s to=%s"%(fromaddr,toaddr))
         suspect=Suspect(fromaddr,toaddr,tempfilename)
         suspect.recipients=set(sess.recipients)
         return suspect
 
+    def commitback(self,suspect):
+        self.logger.info("Commitback...")
+        self.sess.answer=self.sess.Continue()
+        self.sess.finish()
+        self.sess=None
+        
+    def defer(self,reason):
+        #apparently milter wants extended status codes (at least milter-test-server does)
+        if not reason.startswith("5."):
+            reason="4.7.1 %s"%reason
+        self.logger.info("Defer...%s"%reason)
+        self.sess.answer=self.sess.CustomReply(450, reason)
+        self.sess.finish()
+        self.sess=None
+        
+    def reject(self,reason):
+        #apparently milter wants extended status codes (at least milter-test-server does)
+        if not reason.startswith("5."):
+            reason="5.7.1 %s"%reason
+        self.logger.info("reject...%s"%reason)
+        self.sess.answer=self.sess.CustomReply(550, reason)
+        self.sess.finish()
+        self.sess=None
+        
+    def discard(self,reason):
+        self.sess.answer=self.sess.Discard()
+        self.sess.finish()
+        self.sess=None
+        
 class MilterSession(PpyMilter):
     def __init__(self,socket,config):
         PpyMilter.__init__(self)
@@ -65,13 +95,17 @@ class MilterSession(PpyMilter):
         self.logger=logging.getLogger('fuglu.miltersession')
         
         self.__milter_dispatcher = PpyMilterDispatcher(self)
-        self.body=None
         self.recipients=[]
         self.from_address=None
         self.to_address=None
 
-        #TODO: write msg...
-        self.tempfilename=None
+        (handle,tempfilename)=tempfile.mkstemp(prefix='fuglu',dir=self.config.get('main','tempdir'))
+        self.tempfilename=tempfilename
+        self.tempfile=os.fdopen(handle,'w+b')
+        
+        self.currentmilterdata=None
+        
+        self.answer=self.Continue()
 
     def OnRcptTo(self,cmd,rcpt_to,esmtp_info):
         self.recipients.append(rcpt_to)
@@ -81,38 +115,82 @@ class MilterSession(PpyMilter):
     def OnMailFrom(self,cmd,mail_from,args):
         self.from_address=mail_from
         return self.Continue()
+    
+    def OnHeader(self,cmd, header,value):
+        self.tempfile.write("%s: %s\n"%(header,value))
+        return self.Continue()
+    
+    def OnEndHeaders(self,cmd):
+        self.tempfile.write("\n")
+        return self.Continue()
         
     def OnBody(self, cmd, data):
-        self.body=data
+        self.tempfile.write(data)
         return self.Continue()
 
+    def OnEndBody(self, cmd):
+        return self.answer
+
     def OnResetState(self):
-        self.body=None
         self.recipients=None
+        self.tempfile=None
+        self.tempfilename=None
         
+    
+    def _read_milter_command(self):
+        lenbuf=[]
+        lenread=0
+        while lenread < MILTER_LEN_BYTES:
+            pdat=self.socket.recv(MILTER_LEN_BYTES-lenread)
+            lenbuf.append(pdat)
+            lenread+=len(pdat)
+        dat="".join(lenbuf)
+        #self.logger.info(dat)
+        #self.logger.info(len(dat))
+        packetlen = int(struct.unpack('!I',dat)[0])
+        inbuf = []
+        read = 0
+        while read < packetlen:
+            partial_data = self.socket.recv(packetlen - read)
+            inbuf.append(partial_data)
+            read += len(partial_data)
+        data = "".join(inbuf)
+        return data
+    
+    def finish(self):
+        """we assume to be at SMFIC_BODYEOB"""
+        try:
+            while True:
+                if self.currentmilterdata!=None:
+                    data=self.currentmilterdata
+                    self.currentmilterdata=None
+                else:
+                    data=self._read_milter_command()
+                try:
+                    response = self.__milter_dispatcher.Dispatch(data)
+                    if type(response) == list:
+                        for r in response:
+                            self.__send_response(r)
+                    elif response:
+                        self.__send_response(response)
+                except PpyMilterCloseConnection, e:
+                    #logging.info('Closing connection ("%s")', str(e))
+                    break
+        except Exception,e:
+            #TODO: here we get broken pipe if we're not using self.Continue(), but the milter client seems happy
+            #so, silently discarding this exception for now
+            pass
         
     
     def getincomingmail(self):
         try:
             while True:
-                lenbuf=[]
-                lenread=0
-                while lenread < MILTER_LEN_BYTES:
-                    pdat=self.socket.recv(MILTER_LEN_BYTES-lenread)
-                    lenbuf.append(pdat)
-                    lenread+=len(pdat)
-                dat="".join(lenbuf)
-                self.logger.info(dat)
-                self.logger.info(len(dat))
-                packetlen = int(struct.unpack('!I',dat)[0])
-                inbuf = []
-                read = 0
-                while read < packetlen:
-                    partial_data = self.socket.recv(packetlen - read)
-                    inbuf.append(partial_data)
-                    read += len(partial_data)
-                data = "".join(inbuf)
-                self.logger.debug('  <<< %s', binascii.b2a_qp(data))
+                data=self._read_milter_command()
+                self.currentmilterdata=data
+                (cmd, args) = (data[0], data[1:])
+                if cmd==SMFIC_BODYEOB:
+                    self.tempfile.close()
+                    return True
                 try:
                     response = self.__milter_dispatcher.Dispatch(data)
                     if type(response) == list:
@@ -127,7 +205,7 @@ class MilterSession(PpyMilter):
             exc=traceback.format_exc()
             self.logger.error('Exception in MilterSession: %s %s'%(e,exc))
             return False
-        return True
+        return False
 
     def __send_response(self, response):
         """Send data down the milter socket.
@@ -135,7 +213,7 @@ class MilterSession(PpyMilter):
         Args:
           response: the data to send
         """
-        self.logger.debug('  >>> %s', binascii.b2a_qp(response[0]))
+        #self.logger.debug('  >>> %s', binascii.b2a_qp(response[0]))
         self.socket.send(struct.pack('!I', len(response)))
         self.socket.send(response)
 
