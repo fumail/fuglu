cbd3bc3cd4c7c2759812268f6cb7b254ddfe170a
Oli Schacher <oli@fuglu.org>
2015-03-11 22:29:32 +0100

    improve plugin devel documentation

--- source/conf.py
+++ source/conf.py
@@ -19,6 +19,7 @@ import cloud_sptheme as csp
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.expanduser('~/workspace/fuglu/fuglu/src/'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -27,7 +28,7 @@ import cloud_sptheme as csp
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
