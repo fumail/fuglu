4411b79b1e644062e3c85a3d37bfb2c97862c86c
gryphius <oli@fuglu.org>
2015-03-18 11:01:49 +0100

    update copyright years

--- fuglu/src/fuglu/daemon.py
+++ fuglu/src/fuglu/daemon.py
@@ -1,4 +1,4 @@
-#   Copyright 2011 Oli Schacher
+#   Copyright 2009-2015 Oli Schacher
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
