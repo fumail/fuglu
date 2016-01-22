5635db14503ba202821759e59335eb04c1433942
Tom Hendrikx <tom@whyscream.net>
2016-01-22 20:35:17 +0100

    nicer user addition

--- fpm/after-install.sh
+++ fpm/after-install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-/usr/sbin/adduser --quiet --system --group --home /var/lib/fuglu fuglu
+if ! getent passwd fuglu > /dev/null; then
+    /usr/sbin/adduser --system --group --home /var/lib/fuglu fuglu
+fi
 /usr/bin/install --directory --owner root --group fuglu --mode 0775 /var/log/fuglu
