9b772446eed970a213e5ea540726664f46671fd0
Tom Hendrikx <tom@whyscream.net>
2016-01-22 21:00:22 +0100

    fix defautlt config after install to match ubuntu environment

--- fpm/after-install.sh
+++ fpm/after-install.sh
@@ -3,3 +3,10 @@ if ! getent passwd fuglu > /dev/null; then
     /usr/sbin/adduser --system --group --home /var/lib/fuglu fuglu
 fi
 /usr/bin/install --directory --owner root --group fuglu --mode 0775 /var/log/fuglu
+
+/bin/sed -i \
+    -e 's#user=nobody#user=fuglu#' \
+    -e 's#group=nobody#group=fuglu#' \
+    -e 's#tempdir=/tmp#tempdir=/var/lib/fuglu#' \
+    -e 's#port=3310#port=/var/run/clamav/clamd.ctl#' \
+    /etc/fuglu/fuglu.conf.dist
