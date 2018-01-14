1c67c1fc88ec49ed4d127d8ef6bcb1a48cea9d9d
Oli Schacher <oli.schacher@switch.ch>
2018-01-14 11:18:24 +0100

    update version to 0.7.0

--- fuglu/src/fuglu/__init__.py
+++ fuglu/src/fuglu/__init__.py
@@ -16,4 +16,4 @@
 __all__ = ['bounce', 'core', 'daemon', 'funkyconsole',
            'protocolbase', 'scansession', 'shared', 'stats', 'threadpool']
 
-FUGLU_VERSION = "0.6.6"
+FUGLU_VERSION = "0.7.0"
