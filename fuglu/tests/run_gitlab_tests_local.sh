7a4ec1756d97a3859525c5d6c5840a0e0791c4bb
danBLA <danBLA@users.noreply.github.com>
2018-06-20 10:31:43 +0200

    update help-string for run_gitlab_tests_local script

--- fuglu/tests/run_gitlab_tests_local.sh
+++ fuglu/tests/run_gitlab_tests_local.sh
@@ -16,11 +16,6 @@ display_help(){
    echo " Requirements:"
    echo " 1) docker repo on local host running containing fuglu testing image"
    echo "     - install docker"
-   echo "     - create local registry (https://docs.docker.com/registry/deploying/#run-a-local-registry)"
-   echo "       (docker run -d -p 5000:5000 --restart=always --name registry registry:2)"
-   echo "     - cd to fuglu/develop/docker/centos-7"
-   echo "     - build a container running "buildTagPush.sh". This container is used for"
-   echo "       the testing by titlab-runner"
    echo " 2) install gitlab-runner (https://docs.gitlab.com/runner/install/linux-repository.html)"
    echo ""
 }
